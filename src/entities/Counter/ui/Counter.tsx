be275e5af2591780d7cf12460ffb7b0191f51741
Konstantin <you@example.com>
2022-11-06 12:04:27 +0300

    add redux store, add first entity

---
+++ src/entities/Counter/ui/Counter.tsx
@@ -0,0 +1,46 @@
+import { getCounterValue } from 'entities/Counter/model/selectors/getCounterValue/getCounterValue';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button, ButtonTheme } from 'shared/ui/Button/Button';
+import { counterActions } from '../model/slice/counterSlice';
+
+export const Counter = () => {
+    const dispatch = useDispatch();
+    const counterValue = useSelector(getCounterValue);
+
+    const increment = () => {
+        dispatch(counterActions.increment());
+    };
+
+    const decrement = () => {
+        dispatch(counterActions.decrement());
+    };
+
+    return (
+        <div>
+            <h1
+                data-testid="value-title"
+            >
+                {`value = ${counterValue}`}
+            </h1>
+            <Button
+                data-testid="increment-btn"
+                onClick={increment}
+                type="button"
+                theme={ButtonTheme.OUTLINE}
+
+            // eslint-disable-next-line i18next/no-literal-string
+            >
+                INC
+            </Button>
+            <Button
+                data-testid="decrement-btn"
+                onClick={decrement}
+                type="button"
+                theme={ButtonTheme.OUTLINE}
+            // eslint-disable-next-line i18next/no-literal-string
+            >
+                DEC
+            </Button>
+        </div>
+    );
+};
