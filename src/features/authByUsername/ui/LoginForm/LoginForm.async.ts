846e8fe94c6694484c6f4e3f17412f8ff7a946d8
Konstantin <you@example.com>
2022-12-07 23:42:11 +0300

    prepare to production

--- src/features/authByUsername/ui/LoginForm/LoginForm.async.ts
+++ src/features/authByUsername/ui/LoginForm/LoginForm.async.ts
@@ -1,7 +1,4 @@
 import { lazy, FC } from 'react';
 import { LoginFormProps } from './LoginForm';
 
-export const LoginFormAsync = lazy<FC<LoginFormProps>>(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./LoginForm')), 1500);
-}));
+export const LoginFormAsync = lazy<FC<LoginFormProps>>(() => import('./LoginForm'));
