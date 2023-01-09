bdfa1fc88795cd88bafef68f9525b039e2c2226c
Konstantin <timohakt@yandex.ru>
2023-01-09 13:40:07 +0300

    refactoring entities

--- src/entities/Article/index.ts
+++ src/entities/Article/index.ts
@@ -1,7 +1,4 @@
 export { getArticleDetailsData } from './model/selectors/articleDetails';
-
-export { ArticleViewSelector } from './ui/ArticleViewSelector/ArticleViewSelector';
-export { ArticleSortSelector } from './ui/ArticleSortSelector/ArticleSortSelector';
 export {
     articleDetailsReducer,
     articleDetailsActions,
@@ -18,4 +15,3 @@ export {
 export type { ArticleDetailsSchema } from './model/types/articleDetailsSchema';
 export { ArticleList } from './ui/ArticleList/ArticleList';
 export { ArticleListItem } from './ui/ArticleListItem/ArticleListItem';
-export { ArticleTypeTabs } from './ui/ArticleTypeTabs/ArticleTypeTabs';
